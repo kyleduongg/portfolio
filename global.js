950197071ba94c0b036204b9b42c8590ea2a67f9
Kyle Duong <kyduong@ucsd.edu>
2026-04-15 13:51:18 -0700

    added nav menu

--- global.js
+++ global.js
@@ -4,10 +4,29 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-// const navLinks = $$("nav a");
+let pages = [
+  { url: 'index.html', title: 'Home' },
+  { url: 'projects/index.html', title: 'Projects' },
+  { url: 'contact/index.html', title: 'Contact' },
+  { url: 'Resume/index.html', title: 'CV/Resume' },
+  { url: 'https://github.com/kyleduongg', title: 'GitHub'}
+];
 
-//let currentLink = navLinks.find(
-  //(a) => a.host === location.host && a.pathname === location.pathname,
-//);
+let nav = document.createElement('nav');
+document.body.prepend(nav);
 
-//currentLink?.classList.add('current');
+
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+  nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+}
+
+
+const navLinks = $$("nav a");
+
+let currentLink = navLinks.find(
+  (a) => a.host === location.host && a.pathname === location.pathname,
+);
+
+currentLink?.classList.add('current');
