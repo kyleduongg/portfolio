fe3dad99bb1cc78c406c8d29debde897b8a85e7c
Kyle Duong <kyduong@ucsd.edu>
2026-04-15 14:13:44 -0700

    update global.js

--- global.js
+++ global.js
@@ -24,7 +24,10 @@ for (let p of pages) {
   let url = p.url;
   let title = p.title;
   url = !url.startsWith('http') ? BASE_PATH + url : url;
-  nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+  let a = document.createElement('a');
+  a.href = url;
+  a.textContent = title;
+  nav.append(a);
 }
 
 
