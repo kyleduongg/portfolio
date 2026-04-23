bb0254a38b8c901310ba4d0efa62dfafa257cc9a
Kyle Duong <kyduong@ucsd.edu>
2026-04-22 21:16:02 -0700

    project counter

--- projects/projects.js
+++ projects/projects.js
@@ -3,5 +3,9 @@ import { fetchJSON, renderProjects } from '../global.js';
 const projects = await fetchJSON('../lib/projects.json');
 
 const projectsContainer = document.querySelector('.projects');
+const title = document.querySelector('.projects-title');
+
+const count = projects.length;
+title.textContent = `${count} Projects`;
 
 renderProjects(projects, projectsContainer, 'h2');
