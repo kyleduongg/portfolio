321154e109748dda606a5364c75b76b8e573b987
Kyle Duong <kyduong@ucsd.edu>
2026-04-22 21:09:11 -0700

    fixed changes

--- global.js
+++ global.js
@@ -83,20 +83,18 @@ export async function fetchJSON(url) {
   try {
     // Fetch the JSON file from the given URL
     const response = await fetch(url);
-
-    console.log(response);
-
+    console.log(response)
     if (!response.ok) {
       throw new Error(`Failed to fetch projects: ${response.statusText}`);
     }
-
     const data = await response.json();
     return data;
-
   } catch (error) {
     console.error('Error fetching or parsing JSON data:', error);
   }
 }
+
+
 export function renderProjects(projects, containerElement, headingLevel = 'h2') {
   containerElement.innerHTML = '';
 
