b48c55a8b4ee7e5443ab5aeb416bb124f6d0b6e4
Kyle Duong <kyduong@ucsd.edu>
2026-04-28 17:19:54 -0700

    fixed changes

--- projects/projects.js
+++ projects/projects.js
@@ -1,13 +1,10 @@
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
-
 import { fetchJSON, renderProjects } from '../global.js';
 
 const projects = await fetchJSON('../lib/projects.json');
-
 let query = '';
 
 const projectsContainer = document.querySelector('.projects');
-
 renderProjects(projects, projectsContainer, 'h2');
 
 const title = document.querySelector('.projects-title');
@@ -72,28 +69,39 @@ function renderPieChart(projectsGiven) {
       });
   });
 
-    let newLegend = d3.select('.legend');
-    newData.forEach((d, i) => {
+  let newLegend = d3.select('.legend');
+  newData.forEach((d, i) => {
     newLegend
-        .append('li')
-        .attr('class', `legend-item`)
-        .attr('style', `--color:${colors(i)}`)
-        .classed('selected', selectedIndex === i)
-        .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`)
-        .style('cursor', 'pointer')
-        .on('click', () => {
+      .append('li')
+      .attr('class', `legend-item`)
+      .attr('style', `--color:${colors(i)}`)
+      .classed('selected', selectedIndex === i)
+      .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`)
+      .style('cursor', 'pointer')
+      .on('click', () => {
         selectedIndex = selectedIndex === i ? -1 : i;
 
         renderPieChart(projectsGiven);
 
+        
         d3.selectAll('path')
-            .attr('fill', (_, idx) => selectedIndex === idx ? 'oklch(60% 45% 0)' : colors(idx))
-            .classed('selected', (_, idx) => selectedIndex === idx);
+          .attr('fill', (_, idx) => selectedIndex === idx ? 'oklch(60% 45% 0)' : colors(idx))
+          .classed('selected', (_, idx) => selectedIndex === idx);
 
+       
         d3.selectAll('.legend-item')
-            .classed('selected', (_, idx) => selectedIndex === idx); 
-        });
-    });
+          .classed('selected', (_, idx) => selectedIndex === idx);
+
+        if (selectedIndex === -1) {
+          renderProjects(projects, projectsContainer, 'h2');
+        } else {
+          const filteredProjects = projects.filter(
+            (project) => project.year === newData[selectedIndex].label
+          );
+          renderProjects(filteredProjects, projectsContainer, 'h2');
+        }
+      });
+  });
 }
 
 renderPieChart(projects);
