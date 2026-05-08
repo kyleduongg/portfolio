8a890c1628d7c3cf3b1eecbafe8a1b7f0699cc2c
Kyle Duong <kyduong@ucsd.edu>
2026-05-07 23:38:42 -0700

    getting our tooltip back!

--- meta/main.js
+++ meta/main.js
@@ -116,6 +116,8 @@ function updateTooltipPosition(event) {
 
 function createBrushSelector(svg) {
   svg.call(d3.brush());
+
+  svg.selectAll('.dots, .overlay ~ *').raise();
 }
 
 function renderScatterPlot(data, commits) {
