0692ef654d71e9c4c3672bcb1b429eb41475ac6c
Kyle Duong <kyduong@ucsd.edu>
2026-05-18 23:44:27 -0700

    step 2.3

--- meta/main.js
+++ meta/main.js
@@ -342,7 +342,8 @@ function updateFileDisplay(filteredCommits) {
     .groups(lines, (d) => d.file)
     .map(([name, lines]) => {
       return { name, lines };
-    });
+    })
+    .sort((a, b) => b.lines.length - a.lines.length);
 
   let filesContainer = d3
     .select('#files')
